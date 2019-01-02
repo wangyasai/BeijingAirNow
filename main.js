4269d0b2437b3f9f0af9b43f078cf880a4772ec6
wangyasai <471109213@qq.com>
2019-01-02 16:21:32 +0800

    fix

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ var temp17 = [];
 var temp18 = [];
 
 //之后的排序
+var bfRank13 = [];
+var bfRank14 = [];
+var bfRank15 = [];
+var bfRank16 = [];
+var bfRank17 = [];
+var bfRank18 = [];
+
+
+//之后的排序
 var afRank13 = [];
 var afRank14 = [];
 var afRank15 = [];
@@ -84,11 +93,11 @@ var imgs;
 var colors = ["#334d81", "#5071a4","#6b9ac7", "#a1c3d3","#b1b3b4","#8c9395","#697683","#2d2d2d","#F2F2F2"];
 var pmScale = [ 0,15,35,50,100,150,200,300,1000,10000 ];
 var months = ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"]
-var degree = ["0-15：世界卫生组织标准","0-35：中国标准","0-50：国家标准","50-100：良","100-150：一级污染","150-200：二级污染","200-300：三级污染","300-500：四级污染"]
+var degree = ["0-15：世界卫生组织标准","0-35：中国标准","0-50：过渡标准","50-100：二级良","100-150：三级污染","150-200：四级污染","200-300：五级污染","300-500：六级污染"]
 var countDay = [0,32,60,91,121,152,182,213,244,274,305,335];
 var days = [31,28,31,30,31,31,30,31,30,31,30,31];
 var daysline = [0,31,60,91,121,152,182,213,244,274,305,335,365];
-var yearDays = [339,365,365,366,365,360];
+var yearDays = [365,365,365,366,365,365];
 var images = [];
 var bfPosX  = [];
 var bfPosY  = [];
@@ -100,7 +109,7 @@ var img;
 function preload(){
     data = loadTable("2013-2018.csv", "header");
 }
-
+                                                            
 function setup(){
     canvas = createCanvas(windowWidth, 900);
     // canvas.position(0, 180);
@@ -124,7 +133,7 @@ function setup(){
     }
 
      marginL = (windowWidth-barW*365)/2;
- logo = loadImage("img/logo.jpg");
+    logo = loadImage("img/logo.jpg");
     for(var i = 0; i < 366; i++){
          images13[i] = loadImage(data.getString(i,"13img"));
          images14[i] = loadImage(data.getString(i,"14img"));
@@ -135,15 +144,6 @@ function setup(){
      }
 
     
-
-    for(var i = 0; i<rows;i++){
-        bfPosX[i] = marginL + i*barW;
-    }
-
-    for(var i = 0; i < 6;i++){
-        bfPosY[i] = marginT+(barH+spacing)*i;
-    }
-
      for(var i = 0; i < 366; i++){
          temp13[i] = data.getNum(i,"13aqi");
          temp14[i] = data.getNum(i,"14aqi");
@@ -151,7 +151,25 @@ function setup(){
          temp16[i] = data.getNum(i,"16aqi");
          temp17[i] = data.getNum(i,"17aqi");
          temp18[i] = data.getNum(i,"18aqi");
+
+         bfRank13[i] = i;
+         bfRank14[i] = i;
+         bfRank15[i] = i;
+         bfRank16[i] = i;
+         bfRank17[i] = i;
+         bfRank18[i] = i;
      }
+
+
+    for(var i = 0; i<rows;i++){
+        bfPosX[i] = marginL + i*barW;
+        afPosX[i] = marginL + afRank13[j]*barW;
+    }
+
+
+    for(var i = 0; i < 6;i++){
+        bfPosY[i] = marginT+(barH+spacing)*i;
+    }
 
      selection(temp13, rank);
      selection(temp14, rank);
@@ -175,7 +193,7 @@ function setup(){
          afRank17[j] = 365+temp17[j];
          afRank18[j] = 365+temp18[j];
 
-         year13[j] = new SkyBar(bfPosX[j], bfPosY[0], value13, bfPosX[j], marginL+afRank13[j]*barW);
+         year13[j] = new SkyBar(bfPosX[j], bfPosY[0], value13, bfPosX[j], marginL+afRank13[j]*barW);                             
          year14[j] = new SkyBar(bfPosX[j], bfPosY[1], value14, bfPosX[j], marginL+afRank14[j]*barW);
          year15[j] = new SkyBar(bfPosX[j], bfPosY[2], value15, bfPosX[j], marginL+afRank15[j]*barW);
          year16[j] = new SkyBar(bfPosX[j], bfPosY[3], value16, bfPosX[j], marginL+afRank16[j]*barW);
@@ -189,29 +207,33 @@ function draw(){
     background(bgcolor);       
     imageMode(CENTER);
     image(logo,width/2,marginT+barH*9,400,50);
-
+    drawYear();
     if (button == "a") {
       drawMonth();
 
       for(var j = 0; j<6; j++){  
-          for(var i = 0; i < yearDays[j]; i++){
-           year13[i].bfShow();
-           year14[i].bfShow();
-           year15[i].bfShow();
-           year16[i].bfShow();
-           year17[i].bfShow();      
-           year18[i].bfShow();       
+        for(var i = 0; i < yearDays[j]; i++){
+               year13[i].bfShow();
+               year14[i].bfShow();
+               year15[i].bfShow();
+               year16[i].bfShow();
+               year17[i].bfShow();      
+               year18[i].bfShow();       
+            }
         }
-    }
-     if(windowWidth >= 800){
-       for(var j = 0; j<6; j++){  
-            for(var i = 0; i < yearDays[j];i++){         
-             bfBox(bfPosY[j], j, 2, (j+2013));                   
-            } 
-            }       
-             showImg();
-        }
-       
+        if(windowWidth >= 800){
+           for(var j = 0; j<6; j++){  
+                for(var i = 0; i < yearDays[j];i++){     
+                    if(j==3){
+                        bfBox(bfPosY[j], j, 2, (j+2013)); 
+                    }else{
+                        bfBox(bfPosY[j], j, 1, (j+2013)); 
+                    }    
+                                      
+                    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                }       
+                 showImg(bfPosX);
+            }      
     }else if(button == "b") {
         drawDay();	
         for(var i = 0; i < 366; i++){
@@ -222,20 +244,25 @@ function draw(){
            year16[i].afShow();
            year17[i].afShow();
            year18[i].afShow();
+           
         }
+         if(windowWidth >= 800){
+           for(var j = 0; j<6; j++){  
+                for(var i = 0; i < yearDays[j];i++){         
+                        // bfBox(bfPosY[j], j, 2, (j+2013));                   
+                    } 
+                }       
+                 // showImg(afPosX);
+            }                 
     }
-
     noStroke();
     drawBtn(btnY);   
-    drawYear();
     drawLegend(50,100);
-    
-
 }
 
 
-//everyday pm2.5 bar
-function SkyBar(x, y, value, bfX, afX,images){
+//everyday aqi bar
+function SkyBar(x, y, value, bfX, afX){
     this.pos = createVector(x, y);
     this.easing = 0.1;
     this.bfX = bfX;
@@ -255,7 +282,6 @@ function SkyBar(x, y, value, bfX, afX,images){
         if(windowWidth<800){
             rect(this.pos.y-50,this.pos.x+220,barH,barW);
         }else{
-
             rect(this.pos.x, this.pos.y, barW, barH);
         }
         
@@ -281,20 +307,20 @@ function SkyBar(x, y, value, bfX, afX,images){
 }
 
 
-function showImg(){
+function showImg(posX){
     for(var j = 0; j<6;j++){
         for(var i = 0; i<yearDays[j]; i++){   
-            if(mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW && mouseY > bfPosY[0] && mouseY < bfPosY[0]+ barH && j == 0){
+            if(mouseX >  posX[i] && mouseX <  posX[i]+barW && mouseY > bfPosY[0] && mouseY < bfPosY[0]+ barH && j == 0){
                 image(images13[i],mouseX,bfPosY[0]+barH*0.7+80,120,160);
-            }else if( mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW &&mouseY > bfPosY[1] && mouseY < bfPosY[1]+ barH && j == 1){
+            }else if( mouseX > posX[i] && mouseX < posX[i]+barW &&mouseY > bfPosY[1] && mouseY < bfPosY[1]+ barH && j == 1){
                 image(images14[i],mouseX,bfPosY[1]+barH*0.7+80,120,160);
-            }else if( mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW &&mouseY > bfPosY[2] && mouseY < bfPosY[2]+ barH && j == 2){
+            }else if( mouseX > posX[i] && mouseX < posX[i]+barW &&mouseY > bfPosY[2] && mouseY < bfPosY[2]+ barH && j == 2){
                 image(images15[i],mouseX,bfPosY[2]+barH*0.7+80,120,160);
-            }else if( mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW &&mouseY > bfPosY[3] && mouseY < bfPosY[3]+ barH && j == 3){
+            }else if( mouseX > posX[i] && mouseX < posX[i]+barW &&mouseY > bfPosY[3] && mouseY < bfPosY[3]+ barH && j == 3){
                 image(images16[i],mouseX,bfPosY[3]+barH*0.7+80,120,160);
-            }else if( mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW &&mouseY > bfPosY[4] && mouseY < bfPosY[4]+ barH && j == 4){
+            }else if( mouseX > posX[i] && mouseX < posX[i]+barW &&mouseY > bfPosY[4] && mouseY < bfPosY[4]+ barH && j == 4){
                 image(images17[i],mouseX,bfPosY[4]+barH*0.7+80,120,160);
-            }else if( mouseX >  bfPosX[i] && mouseX < bfPosX[i]+barW &&mouseY > bfPosY[5] && mouseY < bfPosY[5]+ barH && j == 5){
+            }else if( mouseX >  posX[i] && mouseX < posX[i]+barW &&mouseY > bfPosY[5] && mouseY < bfPosY[5]+ barH && j == 5){
                 image(images18[i],mouseX,bfPosY[5]+barH*0.7+80,120,160);
             }
         }
@@ -303,31 +329,31 @@ function showImg(){
 
 
 function bfBox(posY, n, lerp,year) {
-        for(var i = 0; i< yearDays[n]; i++){  
-            if (mouseX > bfPosX[i] && mouseX < bfPosX[i]+barW && mouseY > posY && mouseY < posY+ barH) {
-                noStroke();
-                textSize(16);
-                textAlign(LEFT);
-                var value = data.getNum(i, (n+1)*3);
-                var day = data.getString(i, lerp);
+    for(var i = 0; i< yearDays[n]; i++){  
+        if (mouseX > bfPosX[i] && mouseX < bfPosX[i]+barW && mouseY > posY && mouseY < posY+ barH) {
+            noStroke();
+            textSize(16);
+            textAlign(LEFT);
+            var value = data.getNum(i, (n+1)*3+1);
+            var day = data.getString(i, lerp);
 
-                //鼠标移动到某个块，周围变白色         
-                noFill();
-                stroke(255);
-                strokeWeight(1);
-                rect( bfPosX[i], posY, barW, barH);
-                fill(255, 220);
-                strokeWeight(10);
-                stroke(255, 220);
-                rect(mouseX-60,posY+barH*0.7,120,200);
+            //鼠标移动到某个块，周围变白色         
+            noFill();
+            stroke(255);
+            strokeWeight(1);
+            rect( bfPosX[i], posY, barW, barH);
+            fill(255, 220);
+            strokeWeight(10);
+            stroke(255, 220);
+            rect(mouseX-60,posY+barH*0.7,120,200);
 
-                fill(20);              
-                textSize(12);
-                noStroke();
-                text(year + "/" + day, mouseX - 60, posY+barH*0.7 + 170);
-                text("AQI: " + value, mouseX - 60, posY+barH*0.7 + 190);
-                }        
-            }
+            fill(20);              
+            textSize(12);
+            noStroke();
+            text(year + "年" + day, mouseX - 60, posY+barH*0.7 + 170);
+            text("AQI: " + value, mouseX - 60, posY+barH*0.7 + 190);
+            }        
+        }
 }
 
 
@@ -387,12 +413,12 @@ function drawLegend(posY) {
     fill(50);
     textSize(12);
     textAlign(RIGHT);
-    text("好", posX - 2, posY+5);
+    text("优", posX - 2, posY+5);
     textAlign(LEFT);
-    text("糟糕", posX + 8 * w_ + 2, posY+5);
+    text("严重污染", posX + 8 * w_ + 2, posY+5);
 
     if(mouseX > posX  && mouseX <posX + 8 * w_  && mouseY > posY -10 && mouseY < posY +10){
-        fill(255,200);
+        fill(255,230);
         stroke(220);
         strokeWeight(1);
         rect(width/2-w_*4,posY+20,w_*8,180);
@@ -409,6 +435,7 @@ function drawLegend(posY) {
 
     }
 }
+
 
 function drawDay() {
     var b = 7;
